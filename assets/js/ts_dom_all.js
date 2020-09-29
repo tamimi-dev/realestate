4c1347a808c662be49288900cb5de89e7c8b91b3
tamimi-dev <tamimi@users.noreply.github.com>
2020-09-29 05:22:35 -0400

    -

---
+++ assets/js/ts_dom_all.js
@@ -0,0 +1,112 @@
+function fetchCheckStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      var error = new Error(response.statusText);
+      error.response = response;
+      throw error;
+    }
+  }
+  
+  function loadData(url) {
+    var option = {
+      method: "GET",
+      headers: new Headers(),
+      mode: "cors",
+      cache: "default"
+    };
+  
+    return fetch(url, option)
+      .then(fetchCheckStatus)
+      .then(function(resp) {
+        var contentType = resp.headers.get("Content-Type");
+        if (contentType && contentType.indexOf("application/json") !== -1) {
+          return resp.json();
+        } else {
+          return resp.text();
+        }
+      })
+      .then(function(data) {
+        return data;
+      })
+      .catch(function(err) {
+        console.log("Something went wrong! Please check data/schema files");
+      });
+  }
+  
+    var schema = [{
+      "name": "Time",
+      "type": "date",
+      "format": "%-d/%-m/%Y" // NEW fixed format
+    }, {
+      "name": "DOM Durham",
+      "type": "number"
+    }, {
+      "name": "DOM Oshawa",
+      "type": "number"
+    }];
+  
+  
+  var data, dataStore;
+  
+  
+  function formatJSON(entries) {
+      var formattedJSON = [];
+    entries.forEach(item => formattedJSON.push([item.gsx$date.$t, parseFloat(item.gsx$avgdomalldrhm.$t), parseFloat(item.gsx$avgdomallosh.$t)]));
+    return formattedJSON; // NEW return values
+  };
+  
+  Promise.all([
+    loadData(
+      "https://spreadsheets.google.com/feeds/list/1ghkpKiuX7ZdANRb6YhDLt9SgdkrAsxgA_YsMYsker9c/1/public/values?alt=json"
+    )
+  ]).then(function(res) {
+    data = formatJSON(res[0].feed.entry); // NEW added function to format incoming JSON
+  
+    dataStore = new FusionCharts.DataStore(data, schema);
+  
+    new FusionCharts({
+      type: "timeseries",
+      renderAt: "chart-container_dom",
+      id: "variable-height-demo-chart",
+      width: "100%",
+      height: "100%",
+      dataSource: {
+        chart: {
+          multiCanvas: true,
+          canvasHeightProportion: "1:1",
+          "theme": "candy",
+        },
+        caption: {
+          text: "Days on Market"
+        },
+        subcaption: {
+          text: "The average days on market on Durham and Oshawa"
+        },
+        yAxis: [
+          {
+            plot: {
+              value: "Avg Sale Prices",
+              type: "line"
+            },
+            title: "DOM Durham",
+            format: {
+              prefix: "days"
+            }
+          },
+          {
+            plot: {
+              value: "Change in Avg Sale Prices",
+              type: "line"
+            },
+            title: "DOM Oshawa",
+            format: {
+                prefix: "days"
+              }
+          }
+        ],
+        data: dataStore.getDataTable()
+      }
+    }).render();
+  });
+  
