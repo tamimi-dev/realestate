ed061b56ce803ed1ba2ae8ccfaecd20a384ce4be
tamimi-dev <tamimi@users.noreply.github.com>
2020-09-29 03:19:15 -0400

    d

--- assets/js/pie_sale_cnt_drhm_all.js
+++ assets/js/pie_sale_cnt_drhm_all.js
@@ -1,4 +1,4 @@
- $.getJSON("https://spreadsheets.google.com/feeds/list/1ghkpKiuX7ZdANRb6YhDLt9SgdkrAsxgA_YsMYsker9c/2/public/values?alt=json", data => {
+ $.getJSON("https://spreadsheets.google.com/feeds/list/1ghkpKiuX7ZdANRb6YhDLt9SgdkrAsxgA_YsMYsker9c/4/public/values?alt=json", data => {
   var labels = [];
    var seri = [];
   data.feed.entry.forEach(e => {
@@ -28,7 +28,7 @@
           }
         }]
         };
-        var chart = new ApexCharts(document.querySelector("#pieSaleCntDrhmAll"), options);
+        var chart = new ApexCharts(document.querySelector("#pie_sale_cnt_drhm_all"), options);
         chart.render();
     });  
  
